6a9b5849e937acaafef32f2ac037dcf9a850bf65
Lief <williamlief@gmail.com>
2021-09-19 14:54:18 -0700

    Corrected figure to show pre-optimization periods

--- R/09_firstdose_compare_bw_s.R
+++ R/09_firstdose_compare_bw_s.R
@@ -25,7 +25,7 @@ bw_synth <- dat %>%
                   by = c("location" = "State")) %>% 
   mutate(synth = weight * people_vaccinated_per_hundred) %>% 
   group_by(centered_week) %>% 
-  summarize(bw_synth = sum(synth, na.rm = T))
+  summarize(bw_synth = sum(synth, na.rm = T)) 
 
 s_synth <- dat %>% 
   tidylog::left_join(sehgal_weights %>% 
@@ -33,7 +33,7 @@ s_synth <- dat %>%
                      by = c("location" = "State")) %>% 
   mutate(synth = weight * people_vaccinated_per_hundred) %>% 
   group_by(centered_week) %>% 
-  summarize(s_synth = sum(weight * people_vaccinated_per_hundred, na.rm = T))
+  summarize(s_synth = sum(weight * people_vaccinated_per_hundred, na.rm = T)) 
 
 this_synth <- dat %>% 
   tidylog::left_join(this_weights %>% 
@@ -41,39 +41,47 @@ this_synth <- dat %>%
                      by = c("state" = "unit")) %>% 
   mutate(synth = weight * people_vaccinated_per_hundred) %>% 
   group_by(centered_week) %>% 
-  summarize(t_synth = sum(weight * people_vaccinated_per_hundred, na.rm = T))
+  summarize(t_synth = sum(weight * people_vaccinated_per_hundred, na.rm = T)) 
 
 oh <- dat %>% 
   filter(state == "OH") 
 
 compare <- oh %>% 
-  left_join(bw_synth) %>% 
-  left_join(s_synth) %>% 
-  left_join(this_synth) %>% 
+  left_join(bw_synth, by = "centered_week") %>% 
+  left_join(s_synth, by = "centered_week") %>% 
+  left_join(this_synth, by = "centered_week") %>% 
   mutate(`Barber-West` = people_vaccinated_per_hundred - bw_synth, 
          `Sehgal` = people_vaccinated_per_hundred - s_synth,
          `This Paper` = people_vaccinated_per_hundred - t_synth) %>% 
-  pivot_longer(c(`Barber-West`, `Sehgal`, `This Paper`), names_to = "estimate")
+  pivot_longer(c(`Barber-West`, `Sehgal`, `This Paper`), names_to = "estimate") %>% 
+  mutate(optim_start = case_when(
+    estimate == "This Paper" ~ 2, 
+    estimate == "Barber-West" ~ 8,
+    estimate == "Sehgal" ~ 15,
+  ))
 
-ggplot(compare, 
-       aes(x = last_day, y = value, color = estimate)) +
-  geom_line() +
+ggplot(mapping =  
+         aes(x = last_day, y = value, color = estimate)) +
+  geom_line(data = filter(compare, week >=  optim_start), linetype = 1) +
+  geom_line(data = filter(compare, week <= optim_start), linetype = 3) +
   theme_bw() +
   geom_label(aes(x = make_date(2021, 4, 1), y = 1),
              label = "Pre-Lottery Announcment", color = "black") +
-  geom_vline(xintercept = make_date(2021, 5, 12), linetype = "solid") +
+  geom_vline(xintercept = make_date(2021, 5, 12), linetype = "dashed") +
   geom_label(aes(x = make_date(2021, 5, 12) + 20, y = 1), 
                 label = "Lottery Period", color = "black") +
   geom_vline(xintercept = make_date(2021, 6, 23), linetype = "dashed") +
+  geom_hline(yintercept = 0, linetype = "dashed") +
   geom_label(aes(x = make_date(2021, 6, 23) + 18, y = 1), 
                 label = "Post Lottery", color = "black") +
   labs(x = "Data Aggregated to Week Level", 
        y = "Difference in First Doses (OH - Synthetic OH)", 
        color = "Effect Estimate", 
        title = "Extended Time Analysis of First Dose Effects", 
-       subtitle = "Observed Ohio First Dose Vaccination Rate Minus Synthetic Counterfactual")
+       subtitle = "Observed Ohio First Dose Vaccination Rate Minus Synthetic Counterfactual", 
+       caption = "Dotted lines indicate pre-treatment estimates from before the optimization window, worse fit is expected")
 
 ggsave(here("figures/first_dose_compare.jpg"), 
-       bg = "white", width = 7, height = 7)
+       bg = "white", width = 11, height = 7)
 
 compare  %>% filter(centered_week>0) %>% group_by(estimate) %>% summarise(mean(abs(value)^2))
